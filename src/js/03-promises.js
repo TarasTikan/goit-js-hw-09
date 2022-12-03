532e73dcb3d0987843cd7ca2b2fc6fcaf95d2979
TarasTikan <tikantaras1@gmail.com>
2022-12-03 21:58:29 +0200

    add addad

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const formEl = document.querySelector('.form')
+console.log(formEl.elements)
+
+formEl.addEventListener('submit', onAddProm)
+function onAddProm (e) {
+  e.preventDefault()
+
+  let delay = Number(formEl.delay.value);
+  let position = Number(formEl.amount.value);
+  let step = Number(formEl.step.value)
+  for(let i = 1; i <= position; i += 1){
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+  } 
 }
+
+function createPromise(position, delay) {
+  const form = {
+    position,
+    delay
+}
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      if (shouldResolve) {
+        resolve(form)
+      } else {
+        reject(form)
+      }
+    }, delay)
+  })
+}
+
+
